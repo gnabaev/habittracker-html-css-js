b136c0758358d349e5850150636aec442ebf7fe5
gnabaev <gnabaev@gmail.com>
2023-12-23 21:10:33 +0300

    Реализована работа бокового меню.

--- scripts/app.js
+++ scripts/app.js
@@ -3,6 +3,11 @@
 let habits = [];
 const HABIT_KEY = 'HABIT_KEY';
 
+/* page */
+const page = {
+    menu: document.querySelector('.menu__list')
+}
+
 function loadData() {
     const habitsString = localStorage.getItem(HABIT_KEY);
     const habitArray = JSON.parse(habitsString);
@@ -15,6 +20,44 @@ function saveData() {
     localStorage.setItem(HABIT_KEY, JSON.stringify(habits));
 }
 
+/* render */
+
+function rerenderMenu(activeHabit) {
+    if (!activeHabit) {
+        return;
+    }
+
+    for (const habit of habits) {
+        const existing = document.querySelector(`[menu-habit-id="${habit.id}"]`);
+        if (!existing) {
+            const element = document.createElement('button');
+            element.setAttribute('menu-habit-id', habit.id);
+            element.classList.add('menu__item');
+            element.addEventListener('click', () => rerender(habit.id));
+            element.innerHTML = `<img src="/images/${habit.icon}.svg" alt="${habit.name}">`
+            if (activeHabit.id === habit.id) {
+                element.classList.add('menu__item_active');
+            }
+            page.menu.appendChild(element);
+            continue;
+        }
+
+        if (activeHabit.id === habit.id) {
+            existing.classList.add('menu__item_active');
+        } else {
+            existing.classList.remove('menu__item_active');
+        }
+    }
+}
+
+function rerender(activeHabitId) {
+    const activeHabit = habits.find(habit => habit.id === activeHabitId);
+    rerenderMenu(activeHabit);
+}
+
+/* init */
+
 (() => {
     loadData();
-})()
+    rerender(habits[0].id);
+})();
